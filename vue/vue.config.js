1006aec0ab7b7b8f9bb63a1266a02c96e25484ba
李铭昕 <715557344@qq.com>
2021-09-21 19:32:55 +0800

    Update

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api/*': {
+                // http://127.0.0.1:8081/api/user => http://127.0.0.1:9501/user
                 target: 'http://127.0.0.1:9501',
                 changeOrigin: true,
                 pathRewrite: {
