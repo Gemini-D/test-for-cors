4c91d43c558dd403641cded41bdf334c8c8e3054
李铭昕 <715557344@qq.com>
2021-09-21 17:35:35 +0800

    Update

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -21,25 +21,24 @@ class CorsMiddleware implements MiddlewareInterface
 {
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        // $response = Context::get(ResponseInterface::class);
-        // $response = $response->withHeader('Access-Control-Allow-Origin', '*')
-        //     ->withHeader('Access-Control-Allow-Credentials', 'true')
-        //     // Headers 可以根据实际情况进行改写。
-        //     ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
-        //
-        // Context::set(ResponseInterface::class, $response);
-        //
-        // if ($request->getMethod() == 'OPTIONS') {
-        //     return $response;
-        // }
-        //
-        // return $handler->handle($request);
+        $response = Context::get(ResponseInterface::class);
+        $response = $response->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Credentials', 'true')
+            // Headers 可以根据实际情况进行改写。
+            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
+
+        Context::set(ResponseInterface::class, $response);
 
         if ($request->getMethod() == 'OPTIONS') {
-            return $this->handleCORS(Context::get(ResponseInterface::class));
+            return $response;
         }
 
-        return $this->handleCORS($handler->handle($request));
+        return $handler->handle($request);
+        // if ($request->getMethod() == 'OPTIONS') {
+        //     return $this->handleCORS(Context::get(ResponseInterface::class));
+        // }
+        //
+        // return $this->handleCORS($handler->handle($request));
     }
 
     protected function handleCORS(ResponseInterface $response): ResponseInterface
